b96cb0dce8c991dbad037eca6c9bb51d9886412a
Elena1551 <92458199+Elena1551@users.noreply.github.com>
2023-11-15 14:31:45 +0100

    add filter for low frequency

--- check_transient.py
+++ check_transient.py
@@ -1,86 +1,101 @@
-###################
-# IMPORT PACKAGES #
-###################
-
-from collections import defaultdict
-from datetime import datetime, time, timedelta
-import glob
-import grand.dataio.root_trees as rt # GRANDLIB
-import itertools
-import numpy as np
-np.set_printoptions(threshold=np.inf)
-import matplotlib.dates as mdates
-import matplotlib.pyplot as plt
-plt.rc('font', size=10)
-plt.rcParams["font.family"] = "Times New Roman"
-plt.rcParams["axes.labelsize"] = 14
-plt.rcParams['mathtext.default'] = 'regular'
-plt.rcParams['xtick.direction'] = 'in'
-plt.rcParams['ytick.direction'] = 'in'
+# system
 import os
-import time as wall_time
-start_time = wall_time.perf_counter()
+import glob
+import argparse
+from datetime import datetime, time, timedelta
+
+# scipy
+from scipy.signal import butter, filtfilt
+import numpy as np
+import matplotlib.pyplot as plt
+np.set_printoptions(threshold=np.inf)
+# grandlib
+import grand.dataio.root_trees as rt
+
+# time
+import time
+start_time = time.perf_counter()
+
+##############################
+# SET CUSTOM PLOT PARAMETERS #
+##############################
+
+plt.rcParams.update({'axes.labelsize': 30})
+plt.rcParams.update({'xtick.labelsize': 30})
+plt.rcParams.update({'ytick.labelsize': 30})
+plt.rcParams.update({'axes.titlesize': 50})
+plt.rcParams.update({'legend.fontsize': 30})
 
 ####################
 # DEFINE ARGUMENTS #
 ####################
 
-channel_list        = ['X', 'Y', 'Z'] # make dictionaries for easier indexing
-data_dir            = 'data/20231027' # path of data directory containing ROOT files to check
-specific_file       = 'GRAND.TEST-RAW-10s-ChanXYZ_20dB_11DUs_RUN80_test.20231027122253.121_dat.root' # specify which ROOT file to check
-num_samples         = 2048 # number of samples in a trace
+channels            = ['X', 'Y', 'Z'] # make dictionaries for easier indexing
+data_dir            = 'granddata/' # path of data directory containing ROOT files to analyze
+specific_files      = 'GRAND.TEST-RAW-10s-ChanXYZ_20dB_11DUs_RUN80_test.20231027122253.121_dat.root' #specific file
+specific_date       = specific_files.split('.')[-3][4:12]
+empty_channel       = 0
+num_crossings       = 2 # least number of threshold crossings in a time window
 num_threshold       = 5 # trigger threshold of the transient (times of noises)
-noise_list          = [20.0, 20.0, 40.0] # average noise level for 3 ADC channels (ADC counts)
-standard_separation = 100 # required separation between two pulses in a trace (sample numbers)ss
-time_step           = 2 # time step of each sample (ns)
+noises              = [20.0, 20.0, 40.0] # average noise level for 3 ADC channels (ADC counts)
+standard_separation = 100 # required separation between two pulses in a trace (sample numbers)
 
-##############################
-# GET ROOT FILES AND DU LIST #
-##############################
+cutoff_frequency = 30.0  #filter out what's below, in MHz
+sampling_rate = 500.0 #sample every 2 ns
 
-# get ROOT files and their information to analyze
-file_list = sorted(glob.glob(os.path.join(data_dir, specific_file)))
-num_files = len(file_list)
 
-# create an empty set to store unique DUs
-du_set = set()
+#####################################
+# GET ROOT FILES AND DU INFORMATION #
+#####################################
 
-# loop through all files to get used DUs
-for file in file_list:
-    tadc = rt.TADC(file) # initiate TADC tree of this file
-    tadc.get_entry(0) # get the entry from this file
-    used_dus = tadc.get_list_of_all_used_dus() # get used DUs from this file
-    du_set.update(used_dus) # update the set
+# check if the data directory or the file list is empty
+if not os.path.exists(data_dir):
+    raise FileNotFoundError(f'\nData directory is not found: {data_dir}')
+files = sorted(glob.glob(os.path.join(data_dir, specific_files)))
 
-# convert the set to a list in order
-du_list = sorted(list(du_set))
+if not files:
+    raise ValueError("\nProvided specific file list is empty. Please ensure that you provide a non-empty file list.")
 
-# print the list of all used DUs
-print(f'\nROOT files contain data from following DUs: \n{du_list}\n')
+# initiate TADC tree of the file list to get basic info
+total_entries   = 0 # total number of entries across all files
+max_dus         = 0 # maximum used DUs
+id_max_dus_file = 0 # index of file that has maximum used DUs
+for i, file in enumerate(files):
+    tadc = rt.TADC(file)
+    tadc.get_entry(0) # get the entry from current file
+    total_entries += tadc.get_number_of_entries()
+    cur_dus = len(tadc.get_list_of_all_used_dus()) # get used DUs from current file
+    if cur_dus > max_dus:
+        max_dus         = cur_dus
+        id_max_dus_file = i
 
-#######################################
-# CORE FUNCTIONS TO SEARCH TRANSIENTS #
-#######################################
+# get list of used DUs
+tadc = rt.TADC(files[id_max_dus_file])
+du_list = tadc.get_list_of_all_used_dus()
+print(f'\nROOT files contain data from following DUs: {du_list}')
 
-def std_search(trace, num_threshold, standard_separation): 
+# create a channel mask to enable 3 ADC channels and disable channel 0 for GP13
+mask_channel = np.ones(4, dtype=bool)
+mask_channel[empty_channel] = False
+
+def search_windows(trace, threshold, standard_separation):
     # stop if there are no transients
-    threshold = num_threshold * np.std(trace)
     exceed_threshold = np.abs(trace) > threshold
-    if not np.any(exceed_threshold):
+    if np.sum(exceed_threshold) < num_crossings:
         return []
     
     # find trace positions where threshold is exceeded
     crossing_ids = np.flatnonzero(exceed_threshold)
     
     # find the separations between consecutive threshold crossings
-    separations_crossings = np.diff(crossing_ids)
+    crossing_separations = np.diff(crossing_ids)
 
     # locate pulse indices in threshold crossing indices
-    pulse_ids = np.flatnonzero(separations_crossings > standard_separation)
+    pulse_ids = np.flatnonzero(crossing_separations > standard_separation)
     pulse_ids = np.concatenate(([-1], pulse_ids, [len(crossing_ids)-1]))
     
     # preallocate the return list for time windows
-    window_list = [[0, 0] for _ in range(len(pulse_ids) - 1)]
+    window_list = []
 
     # search all transients/pulses
     half_separation = standard_separation // 2
@@ -93,49 +108,15 @@ def std_search(trace, num_threshold, standard_separation):
         stop_id = crossing_ids[pulse_ids[i+1]] + half_separation
         stop_id = min(len(trace)-1, stop_id) # fix the last pulse
 
-        window_list[i] = [start_id, stop_id]
+        # check if this time window contains enough crossings
+        if np.sum(exceed_threshold[start_id:stop_id+1]) >= num_crossings:
+            window_list.append([start_id, stop_id])
 
     return window_list
 
-def noise_search(trace, noise, num_threshold, standard_separation): 
-    # stop if there are no transients
-    threshold = num_threshold * noise
-    exceed_threshold = np.abs(trace) > threshold
-    if not np.any(exceed_threshold):
-        return []
-    
-    # find trace positions where threshold is exceeded
-    crossing_ids = np.flatnonzero(exceed_threshold)
-    
-    # find the separations between consecutive threshold crossings
-    separations_crossings = np.diff(crossing_ids)
-
-    # locate pulse indices in threshold crossing indices
-    pulse_ids = np.flatnonzero(separations_crossings > standard_separation)
-    pulse_ids = np.concatenate(([-1], pulse_ids, [len(crossing_ids)-1]))
-    
-    # preallocate the return list for time windows
-    window_list = [[0, 0] for _ in range(len(pulse_ids) - 1)]
-
-    # search all transients/pulses
-    half_separation = standard_separation // 2
-    for i in range(len(pulse_ids)-1):
-        # get the start index of current pulse
-        start_id = crossing_ids[pulse_ids[i]+1] - half_separation
-        start_id = max(0, start_id) # fix the 1st pulse
-
-        # get the stop index of current pulse
-        stop_id = crossing_ids[pulse_ids[i+1]] + half_separation
-        stop_id = min(len(trace)-1, stop_id) # fix the last pulse
-
-        window_list[i] = [start_id, stop_id]
-
-    return window_list
-
-#####################################
-# PLOT TRANSIENTS IN SELECTED FILES #
-#####################################
-
+#########################################
+# COMPUTE AND PLOT MEAN PSD FOR EACH DU #
+#########################################
 # convert GPS times to UTCs; GPS time = UTC + 18s at present
 def gps2utc(gps_times):
     gps2utc_v = np.vectorize(gps2utc1) # vectorize the helper function
@@ -145,139 +126,69 @@ def gps2utc(gps_times):
 def gps2utc1(gps_time):
     leap_seconds = 18 # number of leap seconds since Jan 6th 1980
     return datetime.utcfromtimestamp(gps_time - leap_seconds)
-
-# 
-def plot_trace(traces, gps_time):
-    # convert GPS time to UTC
-    utc_time = gps2utc1(gps_time)
-
-    # create a 3x1 layout for the subplots and adjust figure size
-    fig, axes = plt.subplots(3, 1, figsize=(36, 12), dpi=256)
-
-    # flatten axes array for easier indexing
-    axes = axes.flatten()
-
-    # plot the data per channel
-    for id, channel in enumerate(channel_list):
-        # search time windows
-        windows = std_search(traces[id], num_threshold, standard_separation)
-
-        # locate corresponding axis
-        ax = axes[id]
-
-        # plot the trace
-        ax.plot(time_axis, traces[id], color='blue', label='trace')
-
-        # enable gird
-        ax.grid(True)
-
-        # add horizontal dashed lines at +/-Threshold of the trace
-        threshold = num_threshold * np.std(traces[id])
-        ax.axhline(y=threshold, color='red', linestyle='--', label=f'+/-Threshold={threshold:.2f}')
-        ax.axhline(y=-threshold, color='red', linestyle='--')
-
-        # highlight the time windows
-        for id, window in enumerate(windows):
-            start_id, stop_id = window
-            # label only the 1st axvspan
-            if id == 0:
-                ax.axvspan(start_id, stop_id, color='green', alpha=0.3, label='Time Window(s)')
-            else:
-                ax.axvspan(start_id, stop_id, color='green', alpha=0.3)
-
-            # annotate time windows
-            centre_id = (start_id + stop_id) / 2
-            ylim = ax.get_ylim()
-            ax.text(centre_id, 1.02*ylim[1], f'[{start_id:.1f}, {stop_id:.1f} ]', color='black', fontsize=10, ha='center', va='bottom')
-        
-        # enable ticks on both left and right sides of the plot
-        ax.tick_params(axis='y', labelleft=True, labelright=True)
-
-        # set title on the right-hand side
-        ax.text(1.01, 0.5, f'Channel {channel}', verticalalignment='center', horizontalalignment='left', transform=ax.transAxes, fontsize=20, rotation=-90)
-        
-        # set X-limits
-        ax.set_xlim(-4, 4100)
-
-        # adjust the size of the x-tick and y-tick labels
-        ax.tick_params(axis='x', labelsize=12)
-        ax.tick_params(axis='y', labelsize=12)
-
-        # only set X-tick labels for the last subplot
-        if id < len(channels) - 1:
-            ax.set_xticklabels([])
-        else:
-            # set an x-tick every 100 units
-            xticks = np.arange(min(time_axis), max(time_axis)+1, 250)
-            ax.set_xticks(xticks)
-
-        # add some comments
-        ax.legend(loc='upper right')
-        ax.set_xlabel('Time / ns', fontsize=16)
-        ax.set_ylabel('ADC Counts', fontsize=16)
-        ax.set_title(f'Gaussian Noise Trace with Detected Windows \n Threshold = {num_threshold}, Separation = {standard_separation}', fontsize=20, pad=20)
-
-    # set X label and Y label for entire figure
-    fig.text(0.5, 0.0, 'DunHuang Time in Date and Hour', ha='center', fontsize=20)
-    fig.text(0.0, 0.5, 'Transient Rate / kHz', va='center', rotation='vertical', fontsize=20)
     
-    # add the figure legend
-    fig.legend(custom_lines, ['Sunrise', 'Sunset'], loc='upper right', fontsize=18, bbox_to_anchor=(0.98,1), bbox_transform=plt.gcf().transFigure)
+def high_pass_filter(trace, cutoff_frequency, sampling_rate):
+    nyquist = 0.5 * sampling_rate
+    normal_cutoff = cutoff_frequency / nyquist
+    b, a = butter(4, normal_cutoff, btype='high', analog=False)
+    filtered_trace = filtfilt(b, a, trace)
+    return filtered_trace
 
-    # add a main/super title for the entire figure
-    plt.suptitle(f'Time Evolution of Transient Rates \n DU{du}, Threshold = {num_threshold}, Separation = {standard_separation}', fontsize=24)
 
-    # adjust layout
-    plt.tight_layout(rect=[0.01, 0.01, 0.99, 1.0])
-
-    # save the figure as a PNG file
-    plt.savefig(f'plot/check/DU{du}_threshold{num_threshold}_separation{standard_separation}.png')
-    print(f'Saved: plot/check/_DU{du}_threshold{num_threshold}_separation{standard_separation}.png')
-
-    # close the figure to free up memory
-    plt.close(fig)
-
-# mask channels to disable channel 0 for GP13 data
-mask_channel = np.array([False, True, True, True])
-
-# make dictionaries to avoid looping through all files more than once
-traces_list = {channel: {} for channel in channel_list}
-windows_list = {channel: {} for channel in channel_list}
+# make dictionaries so that you don't have to loop over all files each time you want to analyze a different DU
+channels  = ['X', 'Y', 'Z']
+traces_all = {channel: {} for channel in channels}
+windows = {channel: {} for channel in channels}
 gps_times = {}
+savedir = f'check_transients/{specific_date}-{num_threshold}-filter{cutoff_frequency}/'
+num_files = len(files)
 
-# loop through all DUs to initiate dictionaries with empty lists
-for du in du_list:
-    for channel in channel_list:
-        traces_list[channel][du] = []
-        windows_list[channel][du] = []
-    gps_times[du] = []
-
-# generate corresponding time axis
-time_axis = np.arange(num_samples) * 2 # 1 sample = 2 nanoseconds
-
-# loop through all files
-for i, file in enumerate(file_list):
+for file_id, file in enumerate(files):
     # get info of this file
     tadc        = rt.TADC(file)
     trawv       = rt.TRawVoltage(file)
     num_entries = tadc.get_number_of_entries()
-
     # loop over all events in this file
-    print(f'{i+1}/{num_files}: Looping over {num_entries} events/entries in {file}')
-    for entry in range(num_entries):
+    print(f'\n{file_id+1}/{num_files}: Looping over {num_entries} events in {file}')
+    for entry in range(100):
         # get info of this entry
         tadc.get_entry(entry)
         trawv.get_entry(entry)
 
         # assume that 1 entry corresponds to only 1 DU
         du = tadc.du_id[0]
-    
+        
+        fig, axs = plt.subplots(len(channels), figsize=(40,20),  sharex=True)
+        
+        #get time
+        gps_times = trawv.gps_time[0]
+        utctime = gps2utc(gps_times)
+        hourdiff = timedelta(hours=8)
+        localtime = utctime + hourdiff
+        
+        
         # get traces and search transients
         traces = np.array(tadc.trace_ch[0])[mask_channel]
-        gps_time = trawv.gps_time[0]
-        plot_trace(traces, gps_time)
+        for i, channel in enumerate(channels):
+            filtered_trace = high_pass_filter(traces[i], cutoff_frequency, sampling_rate)
+            window = search_windows(filtered_trace, num_threshold*np.std(filtered_trace), standard_separation)
+            threshold = num_threshold * np.std(filtered_trace)
+            axs[i].plot(np.arange(2048),filtered_trace,label=f'{channel} trace')
+            axs[i].axhline(y=threshold, color='r', linestyle='--', linewidth=2)
+            axs[i].axhline(y=0-threshold, color='r', linestyle='--', linewidth=2)
+            for w in window :
+                if w != []:
+                    start, end = w[0],w[1]
+                    axs[i].axvspan(start, end, color='green', alpha=0.3)
+            axs[i].set_ylabel('Amplitude')
+            axs[i].legend()
 
-# record the running time
-end_time = wall_time.perf_counter()
-run_time = end_time - start_time
-print(f"\nWhole program executed in: {run_time} seconds")
+        plt.xlabel('Sample Number')
+        plt.suptitle(f'DU {du} - {localtime.hour}:{localtime.minute}:{localtime.second} - threshold {num_threshold} - filter{cutoff_frequency}',fontsize = 40)
+        plt.savefig(f'{savedir}DU{du}_threshold{num_threshold}_{localtime.hour}:{localtime.minute}:{localtime.second} - filter{cutoff_frequency}.png')
+        print(f'saved fig:{savedir}DU{du}_threshold{num_threshold}_{localtime.hour}:{localtime.minute}:{localtime.second}_filter{cutoff_frequency}.png')
+        plt.close()
+
+
+
+
