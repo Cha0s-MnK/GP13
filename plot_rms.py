452acccbc3371ff0787008ad1df92892d1244c16
Cha0s_MnK <143253826+Cha0s-MnK@users.noreply.github.com>
2023-11-16 22:09:46 +0800

    roughly done RMS plot

--- plot_rms.py
+++ plot_rms.py
@@ -35,80 +35,34 @@ print(f'\nNPZ files contain data from following DUs: \n{du_list}\n')
 # PLOT RMS TO ANALYZE #
 #######################
 
-# loop through all DUs to plot transient rates for 3 ADC channels
+# loop through all good DUs to load the NPZ file and plot the histograms
 for du in du_list:
-    # create a 3x1 layout for the subplots and adjust figure size
-    fig, axes = plt.subplots(3, 1, figsize=(36, 12), dpi=256)
-
-    # flatten axes array for easier indexing
-    axes = axes.flatten()
-
-    # plot the data per channel
-    for id, channel in enumerate(channels):
-        # reformat DunHuang hours and transient rates to plot
-        hours_list[channel][du] = list(itertools.chain(*hours_list[channel][du]))
-        rates_list[channel][du] = 1+np.array(list(itertools.chain(*rates_list[channel][du])))
-
-        # locate corresponding axis
-        ax = axes[id]
-
-        # scatter points and add vertical lines to make the plot more clearly
-        ax.scatter(hours_list[channel][du], rates_list[channel][du], color='blue', s=9, alpha=0.6)
-        ax.vlines(hours_list[channel][du], 0, rates_list[channel][du], color='blue', linestyle='solid', linewidth=4, alpha=0.75)
-
-        # set axis scales
-        ax.set_yscale('log')
-
-        # set only the lower limit of the y-axis
-        ax.set_ylim(bottom=1)
-
-        # enable gird
-        ax.grid(True)
-
-        # enable ticks on both left and right sides of the plot
-        ax.tick_params(axis='y', labelleft=True, labelright=True)
-
-        # set title on the right-hand side
-        ax.text(1.01, 0.5, f'Channel {channel}', verticalalignment='center', horizontalalignment='left', transform=ax.transAxes, fontsize=20, rotation=-90)
-        
-        # set X-limits
-        ax.set_xlim(datetime(2023, 10, 11, 6), datetime(2023, 10, 31, 18))
-
-        # set the major tick locator to every 12 hours
-        ax.xaxis.set_major_locator(mdates.HourLocator(interval=12))
-
-        # only set X-tick labels for the last subplot
-        if id < len(channels) - 1:
-            ax.set_xticklabels([])
-        else:
-            # set the formatting of the ticks to include the hour
-            ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d %H:%M'))
-
-            # rotate X labels
-            for tick in ax.get_xticklabels():
-                tick.set_rotation(15)
-
-    # set X label and Y label for entire figure
-    fig.text(0.5, 0.0, 'DunHuang Time in Date and Hour', ha='center', fontsize=20)
-    fig.text(0.0, 0.5, 'Transient Rate / kHz', va='center', rotation='vertical', fontsize=20)
+    # load corresponding NPZ file
+    rms_name     = f'DU{du}_threshold{num_threshold}_separation{standard_separation}_crossing{num_crossings}_cutoff{cutoff_frequency}.npz'
+    rms_npz_file = os.path.join(rms_result_dir, rms_name)
+    data         = np.load(rms_npz_file, allow_pickle=True)
     
-    # add the figure legend
-    fig.legend(custom_lines, ['Sunrise', 'Sunset'], loc='upper right', fontsize=18, bbox_to_anchor=(0.98,1), bbox_transform=plt.gcf().transFigure)
-
-    # add a main/super title for the entire figure
-    plt.suptitle(f'Time Evolution of Transient Rates \n DU{du}, Threshold = {num_threshold}, Separation = {standard_separation}, Crossing = {num_crossings}, Cut-off frequency = {cutoff_frequency}, Noises = [{noises[0]}, {noises[1]}, {noises[2]}]', fontsize=24)
-
-    # adjust layout
-    plt.tight_layout(rect=[0.01, 0.01, 0.99, 1.0])
-
+    # create a figure and adjust figure size
+    plt.figure(figsize=(36, 12), dpi=256)
+    plt.suptitle(f'Noise Distribution of DU{du}')
+    
+    # plot a histogram for each channel in subfigures
+    for id, channel in enumerate(channels, 1):
+        plt.subplot(3, 1, id)  # 3 rows, 1 column layout
+        plt.hist(data[f'noise_list{channel}'], bins=50, alpha=0.75, label=f'Channel {channel}')
+        plt.xlabel('Noise value')
+        plt.ylabel('Frequency')
+        plt.title(f'Channel {channel}', fontsize=20)
+        plt.legend()
+    
     # save the figure as a PNG file
-    rms_png_name = f'rate_DU{du}_threshold{num_threshold}_separation{standard_separation}_crossing{num_crossings}_cutoff{cutoff_frequency}.png'
-    rms_png_file = os.path.join(rms_png_dir, rms_png_name)
-    plt.savefig(os.path.join(rate_dir, rate_file))
-    print(f'Saved: {rate_dir}{rate_file}')
+    rms_png_name = f'rms_DU{du}_threshold{num_threshold}_separation{standard_separation}_crossing{num_crossings}_cutoff{cutoff_frequency}.png'
+    rms_png_file = os.path.join(rms_plot_dir, rms_png_name)
+    plt.savefig(rms_png_file)
+    print(f'Saved: {rms_png_file}')
 
     # close the figure to free up memory
-    plt.close(fig)
+    plt.close()
 
 # record the running time
 end_time = wall_time.perf_counter()
